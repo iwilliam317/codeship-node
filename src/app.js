15b79eaf71c2386029a8e13a1eee6d34ab813b02
william <willshinji@gmail.com>
2018-11-12 00:36:41 -0200

    fix for heroku

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-const server = app.listen(3000);
+const server = app.listen(process.env.PORT || 3000)
 
 app.get('/', (req, res) => res.send('Hello Codeship!'));
 
