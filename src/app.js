9927343b41ea80884ee13c2ce1245d8b52d3d7e0
william <willshinji@gmail.com>
2018-11-12 00:28:23 -0200

    quick fix

--- src/app.js
+++ src/app.js
@@ -4,6 +4,6 @@ const app = express();
 
 const server = app.listen(3000);
 
-app.use('/', (req, res) => res.send('Hello Codeship!'));
+app.get('/', (req, res) => res.send('Hello Codeship!'));
 
 module.exports = server;
